094045e1bd438b8125683ace46d814e21f55df31
Nyerca <facciani.e@gmail.com>
2018-04-08 11:03:35 +0200

    Improve example

--- showHelloWorld.js
+++ showHelloWorld.js
@@ -3,9 +3,10 @@ var http = require('http');
 var dt = require('./dateModule');
 
 //createServer creates an HTTP server object
-http.createServer(function (req, res) { 
+http.createServer(function (req, res) { //req argument represents the request from the client, as an object
 	res.writeHead(200, {'Content-Type': 'text/plain'}); //The result is supposed to be displayed as HTML
 	res.write("The date and time are currently: " + dt.myDateTime()); //write a response to the client
+	res.write(req.url);
 	res.end('Hello World!'); //end the response
 	console.log("Response sent"); 
 }).listen(8080); //the server object listens on port 8080
