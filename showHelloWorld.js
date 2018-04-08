4bf80c6f62b06e2fd53b57fd30304360ac80bde3
EnricoFacciani <enrico.facciani@studio.unibo.it>
2018-04-08 10:43:49 +0200

    add comments

--- showHelloWorld.js
+++ showHelloWorld.js
@@ -2,10 +2,11 @@
 var http = require('http');
 var dt = require('./dateModule');
 
+//createServer creates an HTTP server object
 http.createServer(function (req, res) { 
 	res.writeHead(200, {'Content-Type': 'text/plain'}); 
-	res.write("The date and time are currently: " + dt.myDateTime());
-	res.end('Hello World!'); 
+	res.write("The date and time are currently: " + dt.myDateTime()); //write a response to the client
+	res.end('Hello World!'); //end the response
 	console.log("Response sent"); 
-}).listen(8080); 
+}).listen(8080); //the server object listens on port 8080
 console.log("A node web server is running!");
