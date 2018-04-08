ef49bfdddc443862c1d3cf778badbebb16e7215a
Nyerca <facciani.e@gmail.com>
2018-04-08 14:52:08 +0200

    Email send Module

---
+++ EmailSend/send.js
@@ -0,0 +1,34 @@
+//Use the username and password from your selected email provider to send an email. 
+//how to use a Gmail account to send an email
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+var mailOptions = { //To send HTML formatted text in your email, use the "html" property instead of the "text" property
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  html: '<h1>Welcome</h1><p>That was easy!</p>'
+}
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
