1df444f187b5787613d30bcd5b3d9fb06aeb7edc
EnricoFacciani <enrico.facciani@studio.unibo.it>
2018-04-08 10:38:12 +0200

    Create date module

--- showHelloWorld.js
+++ showHelloWorld.js
@@ -1,6 +1,10 @@
+//Include a module
 var http = require('http');
+var dt = require('./dateModule');
+
 http.createServer(function (req, res) { 
 	res.writeHead(200, {'Content-Type': 'text/plain'}); 
+	res.write("The date and time are currently: " + dt.myDateTime());
 	res.end('Hello World!'); 
 	console.log("Response sent"); 
 }).listen(8080); 
