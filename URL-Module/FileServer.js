626dd1a837cd9db22d89f8732dd8a157d5f2552f
Nyerca <facciani.e@gmail.com>
2018-04-08 14:26:07 +0200

    Add packages example

--- URL-Module/FileServer.js
+++ URL-Module/FileServer.js
@@ -3,7 +3,7 @@
 
 var http = require('http');
 var url = require('url');
-var fs = require('fs');
+var fs = require('fs'); //To work with the file system of the computer
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
